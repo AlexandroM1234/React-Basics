a95612652c4d35fe1965e035329a103cbdcce72e
Alex Martinez <alexmartinez0071234@gmail.com>
2022-08-08 22:19:08 -0400

    getting albums from api created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [albums, setAlbums] = useState([]);
+
+  const getAlbums = async () => {
+    return await axios.get("http://localhost:3001/albums").then((res) => {
+      return res;
+    });
+  };
+
+  useEffect(() => {
+    getAlbums().then((res) => {
+      setAlbums(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="App">Hello World</div>
+      {albums.map((album) => (
+        <div key={album.id}>
+          <p>{album.title}</p>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default App;
